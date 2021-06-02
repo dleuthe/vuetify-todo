293323ed5349c5e5c15e3a5632c80018c0a7c6f8
dleuthe <daniel_leuthe@web.de>
2021-06-02 15:46:56 +0200

    First commit

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,6 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about pa-6">
+    <h1>About this to do app</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam asperiores debitis veritatis recusandae voluptatibus consequuntur sint quo qui mollitia laudantium ut libero, ad enim eos iusto inventore, sed eveniet voluptas.</p>
   </div>
 </template>
